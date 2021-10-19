fa7c7e1a12924672edc66b7772667234a46381b9
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-19 17:58:01 +0100

    Add pagination.min.js library

--- src/displayCountries.js
+++ src/displayCountries.js
@@ -2,38 +2,28 @@ import { getStuff, postStuff, countriesAndFlagsURL } from './api-stuff.js';
 
 const countriesGrid = document.querySelector('.countriesGrid');
 
-
-const codeForSingleCountry = (country) => {
-    return `<div class="country">
-                <div class="flag">
-                    <img src="${country.flag}">
-                </div>
-                <div class="name">
-                    <h3>${country.name}</h3>
+const codeForSingleCountry = (country) => `<div class="card country">
+               <img class="card-img-top flag" src="${country.flag}">
+                <div class="card-body">
+                    <h5 class="card-title">${country.name}</h5>
                 </div>
             </div>`;
-}
-function displayAllCountries(arr, sortCrit="a-z", limit=30, filterStr="") {
+const displayAllCountries = (arr, sortCrit = 'a-z', limit = 30, filterStr = '') => {
+  const arrToDisplay = arr.sort((a, b) => {
+    if (sortCrit === 'a-z') return a.name - b.name;
+    if (sortCrit === 'z-a') {
+      return b.name - a.name;
+    }
+    return 1;
+  }).slice(0, limit);
 
-    let arrToDisplay = arr.sort((a, b) => {
-        if(sortCrit=="a-z")
-            return a.name - b.name;
-        else if(sortCrit=="z-a") {
-            return b.name - a.name;
-        }
-        else {
-            return 1;
-        }
-    }).slice(0, limit);
-
-    countriesGrid.innerHTML = arrToDisplay.map(c => codeForSingleCountry(c)).join('');
-}
+  countriesGrid.innerHTML = arrToDisplay.map((c) => codeForSingleCountry(c)).join('');
+};
 
 (async () => {
-    const countriesData = await getStuff(countriesAndFlagsURL);
-    if(countriesData.data)
-    {
-      const countriesArr = countriesData.data;
-      displayAllCountries(countriesArr, "a-z", 30);
-    }
-  })();
+  const countriesData = await getStuff(countriesAndFlagsURL);
+  if (countriesData.data) {
+    const countriesArr = countriesData.data;
+    displayAllCountries(countriesArr, 'a-z', 50);
+  }
+})();
