0add5bc8525f0af0310d11ef12133ba75cc9fb56
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-20 10:07:11 +0100

    Add missing flag: Bonaire, Saint Eustatius and Saba

--- src/missingFlags.js
+++ src/missingFlags.js
@@ -3,6 +3,9 @@ const mf = {
   'Aland Islands': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Flag_of_%C3%85land.svg/780px-Flag_of_%C3%85land.svg.png',
   'American Samoa': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag_of_American_Samoa.svg/1200px-Flag_of_American_Samoa.svg.png',
   Antarctica: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Proposed_flag_of_Antarctica_%28Graham_Bartram%29.svg/1116px-Proposed_flag_of_Antarctica_%28Graham_Bartram%29.svg.png',
+  Bolivia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Flag_of_Bolivia.svg/1200px-Flag_of_Bolivia.svg.png',
+  'Bonaire, Saint Eustatius and Saba': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Flag_of_Bonaire.svg/900px-Flag_of_Bonaire.svg.png',
+
 
 };
 
