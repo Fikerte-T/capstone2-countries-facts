9e8cca8814629e090fec1d3976afe1752c2e8669
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-20 08:01:17 +0100

    Get rid of pagination.js and its css file

--- src/displayCountries.js
+++ src/displayCountries.js
@@ -1,29 +1,34 @@
 import { getStuff, postStuff, countriesAndFlagsURL } from './api-stuff.js';
 
-const countriesGrid = document.querySelector('.countriesGrid');
-
 const codeForSingleCountry = (country) => `<div class="card country">
                <img class="card-img-top flag" src="${country.flag}">
                 <div class="card-body">
-                    <h5 class="card-title">${country.name}</h5>
+                    <div class="nameAndLikes">
+                        <h5 class="card-title">${country.name}</h5>
+                        <h6><i class="far fa-heart"></i><span class="nbLikes" data-ctr-likes="${country.name}">0</span> likes</h6>
+                    </div>
+                    <div class="commentBtn">
+                         <button type="button"><i class="fas fa-comment"></i> Comments and facts </button>
+                    </div>
                 </div>
             </div>`;
-const displayAllCountries = (arr, sortCrit = 'a-z', limit = 30, filterStr = '') => {
+const displayAllCountries = (arr, sortCrit = 'a-z', limit = 300, filterStr = '') => {
   const arrToDisplay = arr.sort((a, b) => {
-    if (sortCrit === 'a-z') return a.name - b.name;
-    if (sortCrit === 'z-a') {
-      return b.name - a.name;
-    }
+    if (a.name < b.name) return -1;
     return 1;
-  }).slice(0, limit);
+  })
+    .slice(0, limit);
 
-  countriesGrid.innerHTML = arrToDisplay.map((c) => codeForSingleCountry(c)).join('');
+  console.log(arrToDisplay);
+  const htmlCode = arrToDisplay.map((el) => codeForSingleCountry(el)).join('');
+  // eslint-disable-next-line no-undef
+  $('#countriesGrid').html(htmlCode);
 };
 
 (async () => {
   const countriesData = await getStuff(countriesAndFlagsURL);
   if (countriesData.data) {
     const countriesArr = countriesData.data;
-    displayAllCountries(countriesArr, 'a-z', 50);
+    displayAllCountries(countriesArr);
   }
 })();
