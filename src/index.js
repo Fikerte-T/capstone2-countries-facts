e8b5bb6cba4ec9b578d255f7bde556660f3d2423
fikerte <tofikertesfaye@gmail.com>
2021-10-20 12:33:06 +0300

    separate functions that get country details

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ const capital = document.querySelector('.capital')
 const currency = document.querySelector('.currency')
 const dialCode = document.querySelector('.dial-code')
 let displayInfo = document.querySelector('.country-info')
-const countryFlagCurrencyDialcode = `${countriesAPIBaseURL}info?returns=currency,flag,unicodeFlag,dialCode`;
-const capitalUrl = 'https://countriesnow.space/api/v0.1/countries/capital';
-const populationUrl = 'https://countriesnow.space/api/v0.1/countries/population/cities';
+// const countryFlagCurrencyDialcode = `${countriesAPIBaseURL}info?returns=currency,flag,unicodeFlag,dialCode`;
+const capitalUrl = `${countriesAPIBaseURL}capital`;
+const populationUrl = `${countriesAPIBaseURL}population/cities`;
+const dialcodeUrl = `${countriesAPIBaseURL}codes`;
+const currencyUrl = `${countriesAPIBaseURL}currency`;
+const flagUrl= `${countriesAPIBaseURL}flag/images}`;
 
 // const countryInfo = async (country) => {
 //     let response = await getStuff(countryFlagCurrencyDialcode);
@@ -28,67 +31,96 @@ const populationUrl = 'https://countriesnow.space/api/v0.1/countries/population/
 //         }
 //     }); 
 // }
-const countryInfo = async (arr) => {
-    
-            displayInfo.innerHTML = `
-            <img class="flag img-fluid rounded mx-auto d-block" src="${arr.flag}" alt="country flag">
-            <h3 class="country-name">${arr.name}</h3>
-            <div class="d-inline-flex justify-content-between">
-                <div class ="m-3">
-                    <p class="capital">Capital: ${capitalData}</p>
-                    <p class="population">Population: ${arr.currency} in year ${arr.currency}</p>
-                </div >
-                <div class ="m-3">
-                    <p class="currency">Currency: ${arr.currency}</p>
-                    <p class="dial-code">Dial-Code: ${arr.dialCode}</p>
-                </div>
-            </div>
-            `;
-            
-    
+const countryInfo = async (countryname) => {
+    const flag = await getFlag(countryname);
+    // const capital = await getCapital(countryname);
+    const currency = await getCurrency(countryname);
+    const dialcode = await getDialcode(countryname);
+    // const population = await getPopulation(capital);
+    displayInfo.innerHTML = `
+    <img class="flag img-fluid rounded mx-auto d-block" src="${flag}" alt="country flag">
+    <h3 class="country-name">${countryname}</h3>
+    <div class="d-inline-flex justify-content-between">
+        <div class ="m-3">
+            <p class="capital">Capital: ${capital}</p>
+            <p class="population">Population: ${population[0].value} in year ${population[0].year}</p>
+        </div >
+        <div class ="m-3">
+            <p class="currency">Currency: ${currency}</p>
+            <p class="dial-code">Dial-Code: ${dialcode}</p>
+        </div>
+    </div>
+    `;   
 }
 
-let cname = 
-    
+let countryname = 
         {
         name: "Bangladesh",
         flag: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Flag_of_Bangladesh.svg"
         }
     ;
-
-const getFlagCurrencyDialcode = async (cname) => {
-    let response = await getStuff(countryFlagCurrencyDialcode);
-    let data = response.data;
-    console.log(data);
-    console.log(data[0])
-    console.log(cname)
-    data.forEach(async cdata => {
-        if(cname.name === cdata.name){
-            getCapital(cname);
-        }
-    })
+const getDialcode = async (countryname) => {
+    let response = await postStuff(dialcodeUrl, {
+        "country": "bangladesh"
+    });
+    let dialcode = response.data.dial_code;
+    console.log(dialcode)
+    return dialcode;
 }
 
-const getCapital = async (cname) => {
-    let response = await postStuff(capitalUrl, 
-        {
-            "country": 'bangladesh'
-        });
-    let capitalData = response.data.capital;
-    console.log(data);
-    console.log(data.capital);
-    await countryInfo(capitalData);
-    getPopulation(capitalData);
+const getCurrency = async(countryname) => {
+    let response = await postStuff(currencyUrl, {
+        "country": "bangladesh"
+    });
+    let currency = response.data.currency;
+    console.log(currency);
+    return currency;
 }
 
+const getFlag = async(countryname) => {
+    let response =  await postStuff(flagUrl, {
+        "country": "bangladesh"
+    });
+    console.log(response)
+    // let flag = response.data.flag;
+    // console.log(flag)
+    // return flag;
+}
+// const getFlagCurrencyDialcode = async (countryname) => {
+//     let response = await getStuff(countryFlagCurrencyDialcode);
+//     let data = response.data;
+//     console.log(data);
+//     console.log(data[0])
+//     console.log(countryname)
+//     data.forEach(async cdata => {
+//         if(countryname.name === cdata.name){
+//             getCapital(countryname);
+//         }
+//     })
+// }
 const getPopulation = async (capital) => {
     let response = await postStuff(populationUrl, {
         "city": capital
     })
-    let population = response.data.populationCounts[0];
+    let population = response.data.populationCounts;
     console.log(population)
-    
+    return population;
 }
-getFlagCurrencyDialcode(cname);
+const getCapital = async (countryname) => {
+    let response = await postStuff(capitalUrl, 
+        {
+            "country": "bangladesh"
+        });
+    let capitalData = response.data.capital;
+    console.log(capitalData);
+    await getPopulation(capitalData);
+    return capital;
+}
 
-getCapital();
+
+countryInfo(countryname);
+// getDialcode();
+// getCapital();
+// getCurrency();
+// getFlag();
+// getPopulation();
