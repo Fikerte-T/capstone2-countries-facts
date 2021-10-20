b6eb3954daf2104a54066acacbdb00439b7cc278
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-20 09:58:35 +0100

    Add missing flag: Antarctica

--- src/missingFlags.js
+++ src/missingFlags.js
@@ -2,6 +2,8 @@ const mf = {
   Afghanistan: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Flag_of_Afghanistan_%282013%E2%80%932021%29.svg/1200px-Flag_of_Afghanistan_%282013%E2%80%932021%29.svg.png',
   'Aland Islands': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Flag_of_%C3%85land.svg/780px-Flag_of_%C3%85land.svg.png',
   'American Samoa': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag_of_American_Samoa.svg/1200px-Flag_of_American_Samoa.svg.png',
+  Antarctica: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Proposed_flag_of_Antarctica_%28Graham_Bartram%29.svg/1116px-Proposed_flag_of_Antarctica_%28Graham_Bartram%29.svg.png',
+
 };
 
 // eslint-disable-next-line import/prefer-default-export
