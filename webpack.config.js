58077eb902a085b463b4cda972ba4fad84957619
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-23 13:28:37 +0100

    Change mode back to production in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
   devServer: {
     static: './dist',
   },
